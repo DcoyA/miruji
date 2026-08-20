8ffac1dfce4d7e35698f08384398aa8af9c53998
MoneyShot <53429471+DcoyA@users.noreply.github.com>
2026-08-20 21:48:01 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import { withSentryConfig } from "@sentry/nextjs";
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withSentryConfig(nextConfig, {
+  silent: true,
+  org: "여기에_Sentry_조직_슬러그",
+  project: "여기에_Sentry_프로젝트명",
+});
